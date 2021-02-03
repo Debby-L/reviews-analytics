139f97c9b8706e93b266cd96e5a5f331fbd1cafb
Debby Lai <debbylai1229@gmail.com>
2021-02-03 17:34:17 +0800

    final version

--- read.py
+++ read.py
@@ -9,4 +9,9 @@ with open ('reviews.txt', 'r') as f:
 		if count % 1000 == 0: #設定1000的餘數等於0
 		   print(len(data))
 
-print(data[0])
+print('檔案讀取完了, 總共有', len(data), '筆資料')
+
+sum_len = 0
+for d in data: #從清單中調出每筆資料的長度
+    sum_len = sum_len + len(d) #加總全部資料長度
+print ('留言的平均長度為', sum_len/len(data)) #已知共有1000000筆資料
